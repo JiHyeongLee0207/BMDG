69229eeebe7deff4bed2e8d340665e7116d9f0d0
Zbro0207 <wptvkdldj@gmail.com>
2024-06-04 23:50:16 +0900

    [추가,수정] 1.js

--- routes/1.js
+++ routes/1.js
@@ -15,8 +15,6 @@ router.get('/1', async (req, res, next) => {
     
     const MONGO_URI = process.env.MONGO_URI;
     const client = new MongoClient(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    // 가장 높은 input_no 값을 가져와서 다음 값을 설정
     const db = await connectDB(client);
     const collection = await connectBMDG(db);
     const query = await collection.findOne();
@@ -26,26 +24,39 @@ router.get('/1', async (req, res, next) => {
     `;
     const search = `
     <form id="yearForm" method="get">
-        <select name="year" id="year">
-            <option value="" ${!selectedYear ? 'selected' : ''} disabled class="hidden">연도선택</option>
-            ${[2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023].map(year => 
-                `<option value="${year}" ${selectedYear == year ? 'selected' : ''}>${year}</option>`
-            ).join('')}
-        </select>
+        <div class="dropdown">
+            <button type="button" class="dropbtn" id="dropdownButton">연도선택</button>
+            <div id="myDropdown" class="dropdown-content">
+                ${[2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023].map(year => 
+                    `<a href="#" onclick="selectYear(event, ${year})">${year}</a>`
+                ).join('')}
+            </div>
+        </div>
+        <input type="hidden" name="year" id="selectedYear">
     </form>
     `;
 
     
     const contents = selectedYear !== undefined ? `
-        
 
 
     
     ` : '';
     const func = `
-    document.getElementById('year').addEventListener('change', function() {
-        document.getElementById('yearForm').submit();
-    });
+    function selectYear(event, year) {
+        event.preventDefault(); // 기본 링크 동작을 막음
+        
+        // 선택된 연도를 hidden input에 설정
+        document.getElementById('selectedYear').value = year;
+        
+        // 버튼 텍스트를 선택된 연도로 변경
+        document.getElementById('dropdownButton').innerText = year;
+
+        // URL 쿼리 문자열을 변경
+        const url = new URL(window.location);
+        url.searchParams.set('year', year);
+        window.history.pushState({}, '', url);
+    }
     `;
 
     closeConnection(client);
--- routes/mainpage.js
+++ routes/mainpage.js
@@ -9,10 +9,31 @@ router.get('/', (req, res, next) => {
         <link rel="stylesheet" href="../css/main.css">
     `;
     const search = `
-        <p style="text-align:center">This is BMDG</p>
+        <p style="text-align:center">위의 메뉴를 선택해주세요.</p>
     `;
     const contents = `
-        
+        <div>
+            <h1>환영합니다!</h1>
+            <br>
+            <p>해당 사이트는 대학교 프로젝트를 거쳐 만들어졌습니다.</p>
+            <p>서울 열린 데이터 광장 포털에 있는 공공데이터 중, 대한민국 서울시에서 실제로 거래된 부동산에 대한 정보를 모아서 그래프로 시각화한 페이지입니다.</p>
+            <p>현재 사이트에 있는 정보보다 더 많은 정보를 추가되길 원하신다면 아래의 연락처를 확인하시고 이메일로 연락 바랍니다.</p>
+        </div>
+        <div class="team">
+        <h1>웹페이지 창시자</h1>
+            <div class="team-inner">
+                <div>
+                    <img src="./image/chan.png" alt="이찬">
+                    <p>이찬</p>
+                    <p><b>Email</b> ichan0318@naver.com</p>
+                </div>
+                <div>
+                    <img src="./image/jihyeong.png" alt="이지형">
+                    <p>이지형</p>
+                    <p><b>Email</b> wptvkdldj@gmail.com</p>
+                </div>
+            </div>
+        </div>
     `
     res.send(template.make_page(css,search,contents));
 });
