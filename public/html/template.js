a994cadf5e6c1c1a0ca00a544a83f7c9f7be8a84
Zbro0207 <wptvkdldj@gmail.com>
2024-06-07 15:00:21 +0900

    [수정]

--- public/html/template.js
+++ public/html/template.js
@@ -75,7 +75,7 @@ module.exports = {
 
             <div id="loadingScreen" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background-color:rgba(255,255,255,0.6); z-index:1000;">
                 <div style="position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);">
-                    <h1>Loading . . .</h1>
+                    <h1 style="font-size:60px">Loading . . .</h1>
                 </div>
             </div>
 
